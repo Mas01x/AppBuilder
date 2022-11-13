a2e2acee518916e33bb08ffc3bda9f519e18da78
0x000013 <masonschmelz@yahoo.com>
2022-11-12 21:05:36 -0500

    Just a sync update

--- Moduele/__init__.py
+++ Moduele/__init__.py
@@ -6,16 +6,20 @@ class app():
         win = tk.Tk()
         win.title(title)
         self.win = win
+
     def __IntellisenceFix(self):
         #Make it easer to code
         win = tk.Tk()
         self.win = win
+
     def returnWindow(self):
         """Return the tkinter window object"""
         return self.win
+
     def title(self,title):
         """Title the window with the title arg"""
         self.win.title(title)
+
     def geometry(self,x,y,w,h):
         """Set the windows geometry
         X: The x position on the screen
@@ -29,7 +33,9 @@ class app():
         """Set the windows position to x and y"""
         self.geometry(x,y,self.win.winfo_width(),self.win.winfo_height())
 
-    
+    def text(self,tex,**args):
+        tk.Label(self,text=tex,**args)
+
     def launch(self):
         """Launch the window must be called to create window"""
         self.win.mainloop()
--- TestingMod.py
+++ TestingMod.py
@@ -2,4 +2,5 @@ import Moduele
 if __name__ == "__main__":
     app = Moduele.app("Hi")
     app.position(100,100)
+    app.text("Hello World")
     app.launch()
