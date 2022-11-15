53fc0f7c45d5e9809fb2b77f36152c36b25040ae
0x000013 <118074602+Mas01x@users.noreply.github.com>
2022-11-15 06:53:54 -0500

    Update TestingMod.py

--- TestingMod.py
+++ TestingMod.py
@@ -3,4 +3,7 @@ if __name__ == "__main__":
     app = Moduele.app("Hi")
     app.position(100,100)
     app.text("Hello World")
+    app.pack()
+    app.seperator()
+    app.pack(fill="x",padx=10)
     app.launch()
